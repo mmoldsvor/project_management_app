59573ec232015b9df24e74381adf810deb16c95e
Elias Orrem <elias.orrem@gmail.com>
2022-10-04 22:57:18 +0200

    Started on a projectSuccess page and some stylefiles

--- frontend-app/src/routes/Routes.jsx
+++ frontend-app/src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import App from "../components/App";
 import FrontPage from "../components/FrontPage";
 import ExampleApplication from "../components/ExampleComponent";
 import Header from "../components/Header";
+import ProjectSuccess from "../components/ProjectSuccess";
 
 export const AppRoutes = (
     <Routes>
         <Route key="frontPage" path="/" element={<FrontPage />} />
         <Route key="example" path="/example" element={<ExampleApplication />} />
         <Route key="header" path="/header" element={<Header />} />
+        <Route key="success" path="/success" element={<ProjectSuccess />} />
     </Routes>
 );
