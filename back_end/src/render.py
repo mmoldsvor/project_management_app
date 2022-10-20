85252a4c79aadc82e9ba79913c86159e45d6ab4b
sindrhkv <sindrehk2000@hotmail.com>
2022-10-20 11:52:20 +0200

    Initial implementation of work package graph.

--- back_end/src/render.py
+++ back_end/src/render.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def work_package_graph(graph):
     x = [i for i in range(max(node.early_finish for node in graph.end_nodes) + 2)]
@@ -13,10 +13,10 @@ def work_package_graph(graph):
                 y[node.name].append(0)
     return x,y
 
-def temp_render(x, y):
-    plt.stackplot(x, *y.values(), step = 'pre', alpha = 0.7, linewidth = 1, edgecolors = 'black')
-    plt.legend(y.keys())
-    plt.ylim(bottom=0)
-    plt.ylabel("Resources")
-    plt.xlabel("Days")
-    plt.show()
+#def work_package_render(x, y):
+#    plt.stackplot(x, *y.values(), step = 'pre', alpha = 0.7, linewidth = 1, edgecolors = 'black')
+#    plt.legend(y.keys())
+#    plt.ylim(bottom=0)
+#    plt.ylabel("Resources")
+#    plt.xlabel("Days")
+#    plt.show()
