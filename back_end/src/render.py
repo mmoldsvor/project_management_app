c971a5fd6ee9fade453a4a9a14c3d0c985e4e60f
sindrhkv <sindrehk2000@hotmail.com>
2022-11-03 12:29:13 +0100

    Implemented work package plot, returned as svg

--- back_end/src/render.py
+++ back_end/src/render.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from io import StringIO
 
 def work_package_graph(graph):
     x = [i for i in range(max(node.early_finish for node in graph.end_nodes) + 2)]
@@ -11,12 +12,18 @@ def work_package_graph(graph):
                 y[node.name].append(node.resources)
             else:
                 y[node.name].append(0)
-    return x,y
+    return work_package_render(x, y)
 
-#def work_package_render(x, y):
-#    plt.stackplot(x, *y.values(), step = 'pre', alpha = 0.7, linewidth = 1, edgecolors = 'black')
-#    plt.legend(y.keys())
-#    plt.ylim(bottom=0)
-#    plt.ylabel("Resources")
-#    plt.xlabel("Days")
-#    plt.show()
+def work_package_render(x, y):
+    fig = plt.stackplot(x, *y.values(), step = 'pre', alpha = 0.7, linewidth = 1, edgecolors = 'black')
+    plt.legend(y.keys())
+    plt.ylim(bottom=0)
+    plt.ylabel("Resources")
+    plt.xlabel("Days")
+
+    imgdata = StringIO()
+    fig.savefig(imgdata, format = 'svg')
+
+    return imgdata.getvalue()
+
+   
